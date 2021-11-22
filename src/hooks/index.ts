339cca4b1f6f0f121c46a05e6fae7ee5d1d8fae5
李茂峰 <limaofeng@msn.com>
2021-11-22 18:24:04 +0800

    feat: 导出 useSymbols

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useSketch } from './useSketch';
 export { default as useSunmao, useDebugger } from './useSunmao';
 export { default as useComponent } from './useComponent';
 export { default as useReactComponent } from './useReactComponent';
+export { default as useSymbols } from './useSymbols';
